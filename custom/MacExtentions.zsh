9dcf85b832510d87b0b9277b63af018af5b57b7e
Michael W. Clark <MichaelWClark@gmail.com>
2017-04-04 08:56:58 -0500

    Added custom extentions

--- custom/MacExtentions.zsh
+++ custom/MacExtentions.zsh
@@ -2,9 +2,16 @@
 
 #API ALIASES
 alias dapi='~/Projects/denim-api';
-
+alias activate='source ~/Projects/denim-api/.denim-api/bin/activate';
 
 #FRONT-END ALIASES
 alias dfe='~/Projects/denim-app';
-
+alias dcw='celery worker --config=app.data.celeryconfig';
+alias dfw='flower --port=5555 --config=app.data.celeryconfig';
+alias dar='python run.py';
+alias dapidev='rabbitmq-server; dcw && dfw && dar';
 #MAC ALIASES
+alias virtualenv3='~/Library/Python/3.5/bin/virtualenv';
+
+
+PATH=$PATH:/usr/local/sbin
