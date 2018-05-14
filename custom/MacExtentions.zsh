06fba81b4ba7dc92dd6b7b912065372c56ac64a3
Michael W. Clark <MichaelWClark@gmail.com>
2018-05-14 13:11:54 -0500

    Added git flow commands.

--- custom/MacExtentions.zsh
+++ custom/MacExtentions.zsh
@@ -14,3 +14,12 @@ alias dar='python run.py';
 alias dapidev='rabbitmq-server; dcw && dfw && dar';
 #MAC ALIASES
 alias virtualenv3='~/Library/Python/3.5/bin/virtualenv';
+
+alias rel_api_staging='sudo cf target -s staging && sudo cf push -f manifest.staging.yml'
+alias rel_api_prod='sudo cf target -s production && sudo cf push -f manifest.yml'
+alias rel_app_staging='sudo cf target -s staging && yarn run build-staging && sudo cf push -f manifest.staging.yml'
+alias rel_app_prod='sudo cf target -s production && yarn run build && sudo cf push -f manifest.yml'
+
+alias gffs='git flow feature start'
+alias gfff='git flow feature finish `git branch | sed -En "s/\* feature\/(.*)/\1/p"`'
+
